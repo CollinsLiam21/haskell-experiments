a2f41242295ef7a87134924965883ff69e362e00
Liam Collins <licoamll21@hanovernorwichschools.org>
2019-01-29 15:09:21 +0000

    HW1

---
+++ PTfunctions.hs
@@ -0,0 +1,7 @@
+module PTfunctions where
+quadraticVertex a b c = (-b)/(2*a)
+quadraticDiscriminant a b c = (b)**2 - 4*a*c 
+quadraticRealSolutionQty a b c = if quadraticDiscriminant a b c >= 0 then (if quadraticDiscriminant a b c == 0 then 1 else 2) else 0
+convertFtoC f = (f-32)/1.8
+convertCtoF c = (1.8*c) + 32
+convertTemp t string = if string == "c" then convertCtoF t else convertFtoC t
